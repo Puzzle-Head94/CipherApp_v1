d3bf81836e4f9a389f15d3e51f95c895ed7885c1
Puzzle-Head94 <benjamin14brown@gmail.com>
2026-04-15 17:23:10 +0100

    Checkup on app. TODO: javaFX needs to be sorted on VScode. TODO: TBC button activated cipher FIX

--- CypherApp/src/application/Main.java
+++ CypherApp/src/application/Main.java
@@ -9,7 +9,6 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
-
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
